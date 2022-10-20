dcad49e1158660d6cfad314639796c4c2cfa9401
brayden <yancy.brayden@gmail.com>
2022-10-19 21:17:00 -0500

    fisrt commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const morgan = require('morgan')
+const apiRouter = require('./api')
 
-// Setup your Middleware and API Router here
+app.use(morgan('dev'))
+app.use(express.json())
+app.use('/api', apiRouter)
 
 module.exports = app;
