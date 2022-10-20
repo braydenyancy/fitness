581855329edc8a8295b2e98ff5fd2a691e43799a
brayden <yancy.brayden@gmail.com>
2022-10-20 14:36:29 -0500

    QWOKR PLS

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors");
 const app = express()
 const morgan = require('morgan')
 const apiRouter = require('./api')
 
+
+app.use(cors());
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/api', apiRouter)
